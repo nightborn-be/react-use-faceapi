2f9cb9d6d67b524d8105cfc33bbb69c1026351cd
Maxime <>
2020-04-10 00:49:12 +0200

    Updates

---
+++ precache-manifest.c1c2e93d1be73bd581ecdc65a6d509a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8f8d0acbb7a1c853f26943c4907c01",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7f91bea93d988eb6a0d",
+    "url": "./static/css/main.2f4705df.chunk.css"
+  },
+  {
+    "revision": "050010f01319b38e8ef9",
+    "url": "./static/js/2.d5ade11d.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "./static/js/2.d5ade11d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f91bea93d988eb6a0d",
+    "url": "./static/js/main.6f54ef8f.chunk.js"
+  },
+  {
+    "revision": "07c4ca53d9276975b41c50ead07f5fc4",
+    "url": "./static/js/main.6f54ef8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eff3c3e1e9b5ffbc1c0",
+    "url": "./static/js/runtime-main.8137a5a9.js"
+  }
+]);
